db5b09af3c4d9821e8dcc133e10943a62022e1d3
Ace for Safu <safuh8@gmail.com>
2026-08-28 18:11:43 -0400

    fix: format SQLAlchemy imports

--- src/fastapi_backbone/core/database.py
+++ src/fastapi_backbone/core/database.py
@@ -2,7 +2,12 @@
 
 from collections.abc import AsyncIterator
 
-from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
 from sqlalchemy.orm import DeclarativeBase
 
 from .config import Settings
