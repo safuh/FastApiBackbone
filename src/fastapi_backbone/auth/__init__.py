77898708e465f7e0ea5d20d9adc067bc55f8874b
Ace for Safu <safuh8@gmail.com>
2026-09-04 14:04:46 -0400

    feat: export password hasher

--- src/fastapi_backbone/auth/__init__.py
+++ src/fastapi_backbone/auth/__init__.py
@@ -1,5 +1,6 @@
 """Authentication primitives and extension points."""
 
+from .passwords import PasswordHasher
 from .tokens import TokenError, TokenService
 
-__all__ = ["TokenError", "TokenService"]
+__all__ = ["PasswordHasher", "TokenError", "TokenService"]
