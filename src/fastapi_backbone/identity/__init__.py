9b2fdb418ce8b42df1ef5ce24c14322c287b8865
Ace for Safu <safuh8@gmail.com>
2026-09-11 14:35:30 -0400

    feat: export refresh-token persistence

--- src/fastapi_backbone/identity/__init__.py
+++ src/fastapi_backbone/identity/__init__.py
@@ -1,6 +1,12 @@
 """Optional database-backed identity persistence."""
 
 from .models import User
+from .refresh_tokens import RefreshToken, SqlAlchemyRefreshTokenStore
 from .repository import UserCredentialRepository
 
-__all__ = ["User", "UserCredentialRepository"]
+__all__ = [
+    "RefreshToken",
+    "SqlAlchemyRefreshTokenStore",
+    "User",
+    "UserCredentialRepository",
+]
