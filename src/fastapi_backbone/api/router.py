bb9a3eb8c02ac281bc3b9fc1634f2c18722cf242
Ace for Safu <safuh8@gmail.com>
2026-09-11 15:08:20 -0400

    feat: register authentication API router

--- src/fastapi_backbone/api/router.py
+++ src/fastapi_backbone/api/router.py
@@ -2,7 +2,9 @@
 
 from fastapi import APIRouter
 
+from .auth import router as auth_router
 from .health import router as health_router
 
 api_router = APIRouter()
 api_router.include_router(health_router)
+api_router.include_router(auth_router)
